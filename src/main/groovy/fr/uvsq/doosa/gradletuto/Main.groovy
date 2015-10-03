bea560260fd8d989ce550506c5747b3c5e69cd53
uvsq21504690 <hosana.rotsy @gmail.com>
2015-10-03 15:03:42 +0200

    insertion de nouveaux fichiers

---
+++ src/main/groovy/fr/uvsq/doosa/gradletuto/Main.groovy
@@ -0,0 +1,12 @@
+package fr.uvsq.doosa.gradletuto
+
+class Main {
+    static void main(def args) {
+        Main m = new Main()
+        println m.returnHello()
+    }
+
+    String returnHello() {
+        'Hello'
+    }
+}
